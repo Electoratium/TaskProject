6c609552c02619074b73b7f81d4551ae86d8dba9
Electoratium <electoratium@gmail.com>
2019-03-01 21:16:08 +0200

    reinit build

---
+++ build/precache-manifest.253fa711aef17163881ef4e98e031754.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9244fa268e0851242156",
+    "url": "/static/js/main.9244fa26.chunk.js"
+  },
+  {
+    "revision": "303efbd1b5f90c845109",
+    "url": "/static/js/1.303efbd1.chunk.js"
+  },
+  {
+    "revision": "9244fa268e0851242156",
+    "url": "/static/css/main.e4b1c9b6.chunk.css"
+  },
+  {
+    "revision": "303efbd1b5f90c845109",
+    "url": "/static/css/1.8442de25.chunk.css"
+  },
+  {
+    "revision": "6a68c9d74a431f5153c3cfaec1689976",
+    "url": "/index.html"
+  }
+];
